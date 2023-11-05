2e4be43e04fe7aa72ad7bd31cc6d3551c3481981
vlow727272 <149936839+vlow727272@users.noreply.github.com>
2023-11-05 13:24:43 +0300

    Update run.sh
    Н

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+$ apt update
+$ apt upgrade
+$ pkg install git -y
+$ pkg install python
+$ git clone https://github.com/kovinevmv/getcontact
+$ cd getcontact
+$ pip3 install -r requirements.txt
+$ cd src
+$ python main.py -p +79852361739
 
-python3 ./src/main.py "$@"
+
